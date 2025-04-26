01bff30cf7d8f5a68914188f8469393b511d9f95
jjy12163 <>
2025-04-26 16:18:44 +0900

    first commit

---
+++ src/main/java/com/TripCam/gdgoc3/Gdgoc3Application.java
@@ -0,0 +1,13 @@
+package com.TripCam.gdgoc3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gdgoc3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gdgoc3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/TripCam/gdgoc3/Gdgoc3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TripCam.gdgoc3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gdgoc3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
